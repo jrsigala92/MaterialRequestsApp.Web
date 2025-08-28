b1a7716404c7fb1488f09b7175bd10d7d3fde91b
Jesus Sigala <jsigala92@outlook.com>
2025-08-28 17:16:46 -0600

    importing mat menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map, Observable, shareReplay } from 'rxjs';
@@ -22,6 +23,7 @@ import { AuthService } from './services/auth.service';
     MatListModule,
     MatIconModule,
     MatButtonModule,
+    MatMenuModule,
     AsyncPipe,
     CommonModule
   ],
